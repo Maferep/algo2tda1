fd96d7c7bd2b22fac8e787dc5bd998cc1a31711f
Mafer <mf.pont.tv@gmail.com>
2020-05-17 15:52:11 -0300

    Implemented list-related functions

--- lista.c
+++ lista.c
@@ -1,23 +1,39 @@
 #include "lista.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
-#define VERDADERO '1'
-#define FALSO '0'
+#define EXITO 0
+#define FRACASO -1
 typedef struct nodo
 {
     struct nodo* siguiente;
     void* contenido;
 }nodo_t;
 
-typedef struct lista
+struct lista
 {
     nodo_t* inicio;
     size_t tamanio;
-    char esta_vacia;
-}lista_t;
+};
+//TODO DOC devuelve true si posicion está en el rango [0, lista->tamanio] inclusive
+static bool posicion_en_rango(lista_t* lista, size_t posicion)
+{
+    return (posicion<=(lista->tamanio));
+}
+
+//TODO DOC Devuelve true si el tamaño de la lista es 0.
+bool lista_vacia(lista_t* lista) 
+{ 
+    return lista->tamanio == 0; 
+} 
+//Devuelve la cantidad de elementos en la lista
+size_t lista_elementos(lista_t* lista)
+{
+    return (lista->tamanio);
+}
 //TODO-DOC aloca memoria a un nodo, le asigna un contenido y lo hace apuntar a NULL.
 //devuelve el nodo si fue creado. Si falla devuelve NULL.
-nodo_crear(void* contenido)
+static nodo_t* nodo_crear(void* contenido)
 {
     nodo_t* nodo = malloc(sizeof(nodo_t)); 
     if(!nodo) return NULL;
@@ -25,22 +41,26 @@ nodo_crear(void* contenido)
     nodo->siguiente = NULL;
     return nodo;
 }
-//TODO-DOC itera cada nodo [indice] veces 
-nodo_t* lista_acceder_nodo(lista_t* lista, size_t indice)
+//TODO-DOC busca el nodo [indice] y lo devuelve, si no lo encuentra devuelve NULL. 
+//Si la posición provista es inválida, devuelve NULL.
+static nodo_t* lista_acceder_nodo(lista_t* lista, size_t indice)
 {
-    if(indice >= lista->tamanio) return NULL;
+    if(!lista || indice >= lista->tamanio || lista_vacia(lista)) 
+        return NULL;
     nodo_t* nodo_actual = lista->inicio;
-    while(indice)
+    while(indice) //Nodo nunca apunta prematuramente a NULL si el tamanio de la lista es correcto
     {
-        nodo_actual = nodo_actual->siguiente; //Nodo nunca apunta prematuramente a NULL si el tamanio de la lista es correcto
+        nodo_actual = nodo_actual->siguiente; 
         indice--;
     }
     return nodo_actual;
 }
-nodo_t* lista_ultimo_nodo(lista_t* lista)
+//TODO-DOC devuelve el nodo lista->tamanio-1, o NULL si la lista está vacía
+static nodo_t* lista_ultimo_nodo(lista_t* lista)
 {
-    return lista_acceder_nodo(lista, lista->tamanio);
+    return lista_acceder_nodo(lista, (lista->tamanio)-1 );
 }
+
 lista_t* lista_crear()
 {
     lista_t* nueva = malloc(sizeof(lista_t));
@@ -51,13 +71,89 @@ lista_t* lista_crear()
     }
     nueva->inicio = NULL;
     nueva->tamanio = 0;
-    nueva->esta_vacia = VERDADERO;
     return nueva;
+}
+void lista_destruir(lista_t* lista)
+{
+    if(!lista)
+    {
+        printf("Lista no existe\n");
+        return;
+    }
+    nodo_t* auxiliar;
+    nodo_t* actual = lista->inicio;
+    while(actual)
+    {
+        auxiliar = actual->siguiente;
+        free(actual);
+        actual = auxiliar;
+    }
+    free(lista);
+}
+
+int lista_insertar_en_posicion(lista_t* lista, void* elemento, size_t posicion)
+{
+    if(!lista||!posicion_en_rango(lista, posicion)) 
+        return FRACASO;
+    nodo_t* nuevo_nodo = nodo_crear(elemento);
+    if(!nuevo_nodo) 
+        return FRACASO;
+    //DEL lista acceder nodo devuelve NULL si posicion es igual al tope de la lista
+    nuevo_nodo->siguiente = lista_acceder_nodo(lista, posicion); 
+    if(posicion>0) //el nodo anterior apunta al nuevo nodo
+        lista_acceder_nodo(lista, posicion-1)->siguiente = nuevo_nodo;
+    else // inserto en el primer nodo
+        lista->inicio =  nuevo_nodo;
+    (lista->tamanio)++;
+    //DEBUG
+    printf("insertó %c, ahora hay %li elementos\n", *((char*) elemento), lista->tamanio);
+    return EXITO;
 }
 int lista_insertar(lista_t* lista, void* elemento)
 {
-    nodo_t* nuevo_nodo;
-    nodo_crear(elemento);
-    lista_ultimo_nodo(lista)->siguiente = nuevo_nodo;
-    lista->tamanio++;
+    if(lista_insertar_en_posicion(lista, elemento, lista->tamanio)==EXITO)
+        return EXITO;
+    return FRACASO;
 }
+//borra el elemento [posicion]
+int lista_borrar_de_posicion(lista_t* lista, size_t posicion)
+{
+    if(!lista||!posicion_en_rango(lista, posicion) || posicion == lista->tamanio) 
+        return FRACASO;
+    nodo_t* a_borrar;
+    nodo_t* previo = lista_acceder_nodo(lista, posicion-1); //accedo el nodo justo antes del nodo a borrar
+    if(previo)
+    {
+        a_borrar = previo->siguiente;
+    } 
+    else if(posicion==0)
+    {
+        a_borrar = lista->inicio;
+    }
+    else return FRACASO;
+    nodo_t* posterior = a_borrar->siguiente;
+    free(a_borrar);
+    previo->siguiente = posterior;
+    (lista->tamanio)--;
+    return EXITO;
+}
+int lista_borrar(lista_t* lista)
+{
+    return lista_borrar_de_posicion(lista,(lista->tamanio) - 1);
+}
+void* lista_elemento_en_posicion(lista_t* lista, size_t posicion)
+{
+    if(!lista) return NULL;
+    if(posicion_en_rango(lista, posicion) && posicion != lista->tamanio) 
+        return lista_acceder_nodo(lista,posicion)->contenido;
+    else return NULL;
+}
+void* lista_ultimo(lista_t* lista)
+{
+    return lista_acceder_nodo(lista,(lista->tamanio)-1)->contenido;
+}
+int lista_apilar(lista_t* lista, void* elemento)
+{
+    
+}
+
--- lista_minipruebas.c
+++ lista_minipruebas.c
@@ -1,6 +1,7 @@
 #include "lista.h"
 #include <stdio.h>
-
+#define EXITO 0
+#define FRACASO -1
 void mostrar_elemento(void* elemento, void* contador){
   if(elemento && contador)
     printf("Elemento %i: %c \n", (*(int*)contador)++, *(char*)elemento);
@@ -10,23 +11,53 @@ void mostrar_elemento(void* elemento, void* contador){
 
 void probar_operaciones_lista(){
  lista_t* lista = lista_crear();
-  
+  /*
   char a='a', b='b', c='c', d='d', w='w';
   
-  lista_insertar(lista, &a);
-  lista_insertar(lista, &c);
-  lista_insertar_en_posicion(lista, &d, 100);
-  lista_insertar_en_posicion(lista, &b, 1);
-  lista_insertar_en_posicion(lista, &w, 3);
-
+  if(lista_insertar(lista, &a)==FRACASO)
+  {
+    printf("FALLO: no pudo insertar a\n");
+  }
+  if(lista_insertar(lista, &c)==FRACASO)
+  {
+    printf("FALLO: no pudo insertar c\n");
+  }
+   if(lista_insertar_en_posicion(lista, &d, 100)==FRACASO)
+  {
+    printf("FALLO: no pudo insertar d\n");
+  }
+   if(lista_insertar_en_posicion(lista, &b, 1)==FRACASO)
+  {
+    printf("FALLO: no pudo insertar b\n");
+  }
+   if(lista_insertar_en_posicion(lista, &w, 3)==FRACASO)
+  {
+    printf("FALLO: no pudo insertar w\n");
+  }
+  */
+ //CASOS BORDE
+ //mala lista
+  char letras[10] = {"valores y respeto mutuo"};
+  for(int i = 0; i < 10; i++)
+  {
+    lista_insertar(lista,letras[i]);
+  }
+  for(int i = 0; i < 10; i+=3)
+  {
+    lista_borrar_de_posicion(lista, i);
+    lista_insertar_en_posicion(lista,(letras[i])+3,i);
+  }
+  
   lista_borrar_de_posicion(lista, 3);
 
   printf("Elementos en la lista: ");
-  for(size_t i=0;i<lista_elementos(lista);i++)
-    printf("%c ", *(char*)lista_elemento_en_posicion(lista, i));
-
+  for(size_t i=0;i<lista_elementos(lista);i++){
+    if(lista_elemento_en_posicion(lista, i))
+      printf("%c ", *(char*)lista_elemento_en_posicion(lista, i));
+    else printf("ERROR\n");
+  }
   printf("\n\n");
-
+/*
   printf("Imprimo la lista usando el iterador externo: \n");
   lista_iterador_t* it = lista_iterador_crear(lista);
   while(lista_iterador_tiene_siguiente(it))
@@ -38,12 +69,13 @@ void probar_operaciones_lista(){
   int contador=0;
   printf("Imprimo la lista usando el iterador interno: \n");
   lista_con_cada_elemento(lista, mostrar_elemento, (void*)&contador);
-  printf("\n");
+  printf("\n");*/
   
   lista_destruir(lista);
 }
 
 void probar_operaciones_cola(){
+  /*
   lista_t* cola = lista_crear();
 
   int numeros[]={1,2,3,4,5,6};
@@ -59,11 +91,11 @@ void probar_operaciones_cola(){
     lista_desencolar(cola);
   }
   printf("\n");
-  lista_destruir(cola);
+  lista_destruir(cola);*/
 }
 
 void probar_operaciones_pila(){
-  lista_t* pila = lista_crear();
+  /*lista_t* pila = lista_crear();
   char* algo="somtirogla";
 
   for(int i=0;algo[i]!= 0;i++){
@@ -79,6 +111,7 @@ void probar_operaciones_pila(){
   printf("\n");
 
   lista_destruir(pila);
+  /*/
 }
 
 int main(){
